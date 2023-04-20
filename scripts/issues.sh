b67c38f07380dee374cc612db7d4deb11067c96d
Victor Villas <mail@victor.villas>
2023-04-20 11:57:28 -0700

    Add issue curation action and script

---
+++ scripts/issues.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash -u
+set -o nounset
+
+open_issues="$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+    -H "per_page: 100" -H "state: open" \
+    "https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/issues")"
+
+jq --compact-output '.[]' <(echo "$open_issues") |
+    cat
