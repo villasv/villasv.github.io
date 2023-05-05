06714d7d24dd920bc8e8c1ed61aa87c89d577677
Victor <mail@victor.villas>
2023-05-05 15:10:37 -0700

    Add places and www page dirs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Index from "./index.mdx";
 
 export const metadata = {
   title: "Victor's Website",
-  description: "on the independent web.",
+  description: "on the independent web",
 };
 
 export default function Page() {
---
+++ app/places/page.tsx
@@ -0,0 +1,10 @@
+import Places from "./places.mdx";
+
+export const metadata = {
+  title: "Victor's Website",
+  description: "on the independent web.",
+};
+
+export default function Page() {
+  return <Places />;
+}
