8bb7a38b5bd5f5e03e27e4e08fbbafb8e39708f1
Victor Villas <mail@victor.villas>
2023-06-30 16:00:15 -0700

    Expand life stretch texts

--- app/articles/life/page.tsx
+++ app/articles/life/page.tsx
@@ -2,19 +2,28 @@
 
 import { stretchable } from "@/components/stretchtext";
 
-const fulfilling = stretchable(
-  "fulfilling",
-  <>fulfilling means what</>
+const fulfilling = stretchable("fulfilling", <>fulfilling means what</>);
+
+const To = stretchable(
+  "To",
+  <>
+    {stretchable("We", <>All living creatures</>)}{" "}
+    {stretchable("are meant", <>are given life</>)} to
+  </>
 );
-const toBeHappy = stretchable(
-  "to be happy",
-  <>to live a {fulfilling} and cheerful life</>
+
+const be = stretchable("be", <>be</>);
+const happy = stretchable(
+  "happy",
+  <>to live a {fulfilling} and cheerful life full of wonders and stuff and so on</>
 );
 
 export default function Page() {
   return (
     <div>
-      <p>I want {toBeHappy}.</p>
+      <p>
+        {To} {be} {happy}.
+      </p>
     </div>
   );
 }
