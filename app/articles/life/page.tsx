29bd274b597f47a051b09a661dc7f76ed2e02f60
Victor Villas <mail@victor.villas>
2023-07-01 14:00:31 -0700

    Add title to life article

--- app/articles/life/page.tsx
+++ app/articles/life/page.tsx
@@ -15,12 +15,15 @@ const To = stretchable(
 const be = stretchable("be", <>be</>);
 const happy = stretchable(
   "happy",
-  <>to live a {fulfilling} and cheerful life full of wonders and stuff and so on</>
+  <>
+    to live a {fulfilling} and cheerful life full of wonders and stuff and so on
+  </>
 );
 
 export default function Page() {
   return (
     <div>
+      <h1>On Life, the Universe, and Everything</h1>
       <p>
         {To} {be} {happy}.
       </p>
