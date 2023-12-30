6d604064cf59df92a8c99b37e5183a5def9fff42
villasv <villasv@users.noreply.github.com>
2023-12-30 06:31:41 +0000

    deploy: aaf80210f18714a898e195db09713c0967098386

---
+++ _next/static/chunks/app/(aspects)/layout-730e27d8866f113e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[976],{8279:function(e,n,t){Promise.resolve().then(t.t.bind(t,7580,23))},7580:function(e){e.exports={article:"page_article__3REzu"}}},function(e){e.O(0,[971,938,744],function(){return e(e.s=8279)}),_N_E=e.O()}]);
