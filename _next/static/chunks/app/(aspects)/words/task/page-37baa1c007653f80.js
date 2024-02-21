039ab8e2f905ac34a65264ba11285c9503e8d140
villasv <villasv@users.noreply.github.com>
2024-02-21 20:13:12 +0000

    deploy: 8b4ed924e2b7f3fb4c0fa40d71cd805649e98151

---
+++ _next/static/chunks/app/(aspects)/words/task/page-37baa1c007653f80.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[867,414,116,447,623,842,88],{3521:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
