c9ab4ca32fdf8316e2ff1db3c21c9856a527711f
villasv <villasv@users.noreply.github.com>
2024-02-21 20:18:21 +0000

    deploy: b62774cc51b0da002aaf09a06fae4ee0286c5300

--- _next/static/chunks/app/(aspects)/coffee/page-974b467580698166.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[375,886,659,761,86],{3130:function(n,e,u){Promise.resolve().then(u.t.bind(u,2076,23))},2076:function(){}},function(n){n.O(0,[333,971,938,744],function(){return n(n.s=3130)}),_N_E=n.O()}]);
