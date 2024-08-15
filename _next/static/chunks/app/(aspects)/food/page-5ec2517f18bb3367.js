c33b71b2923b82daabf4602e3f79909dc41bedc2
villasv <villasv@users.noreply.github.com>
2024-08-15 02:16:25 +0000

    deploy: 83998079e04d4f2b1960f84afe3458aa4afc6812

---
+++ _next/static/chunks/app/(aspects)/food/page-5ec2517f18bb3367.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[886,827,375,49,471,761,86],{5167:function(n,e,u){Promise.resolve().then(u.t.bind(u,6395,23))},6395:function(){}},function(n){n.O(0,[911,971,23,560],function(){return n(n.s=5167)}),_N_E=n.O()}]);
