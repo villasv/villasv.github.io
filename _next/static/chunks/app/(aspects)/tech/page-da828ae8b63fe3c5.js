ebda35c1322b2bceedf4f526ede27c5a34879899
villasv <villasv@users.noreply.github.com>
2024-10-09 01:23:42 +0000

    deploy: 425a38327fafb9aeef16373ba56a20d3b1461d8d

--- _next/static/chunks/app/(aspects)/tech/page-da828ae8b63fe3c5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[471,827,414,116,744,375,581,297,886,623,640,108,546,842,761],{3521:function(){}},function(n){n.O(0,[971,23,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/(aspects)/tech/stuff/page-b6e397683098d068.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[546,414,116,827,744,581,375,297,886,623,640,108,471,842,761],{3521:function(){}},function(n){n.O(0,[971,23,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
