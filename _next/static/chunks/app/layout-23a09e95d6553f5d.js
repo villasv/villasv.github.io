05d47dc48c45f1e3b232840b7d4896291f9890f1
villasv <villasv@users.noreply.github.com>
2025-01-27 01:40:36 +0000

    deploy: b9e01cd8ba8dc158c0da8dac19ccd74d7b40ee0f

--- _next/static/chunks/app/layout-23a09e95d6553f5d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7435:function(n,e,u){Promise.resolve().then(u.t.bind(u,7960,23))},7960:function(){}},function(n){n.O(0,[587,971,117,560],function(){return n(n.s=7435)}),_N_E=n.O()}]);
